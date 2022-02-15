33226e18989ce85723dbbed4308de718a9f14145
Lil_T0ny <anthony.gomez@epn.edu.ec>
2022-02-14 19:18:30 -0500

    Sam
    Proyecto borrador 1

---
+++ Original Proyecto Omega/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Original_Proyecto_Omega
+{
+    public partial class tltProyectoOmega : Form
+    {
+        public tltProyectoOmega()
+        {
+            InitializeComponent();
+        }
+
+        private void tltProyectoOmega_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
